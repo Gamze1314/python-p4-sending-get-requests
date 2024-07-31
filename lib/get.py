f1b2c8dbca0da8d2f72d719e799351fdf3286469
gamze <gamzekandara@gmail.com>
2024-07-31 17:01:22 -0400

    decode/encode json response

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,20 @@
-import requests
+import requests #coming from Request class.
 import json
 
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
-response = requests.get(url)
+response = requests.get(url) # get() dict method.
 
-json_content = json.loads(response.text)
+json_content = json.loads(response.text)  # Deserialize/decode data as JSON, turns JSON data into list.
 
-print(json.dumps(json_content, indent=4))
+print(json.dumps(json_content, indent=4)) # dumps()  encode/serialize data as JSON.
+
+
+#json -- JSON encoder and decoder.
+#indent=4 => pretty printing: 
+
+# {
+#     "4": 5,
+#     "6": 7
+# }
+
